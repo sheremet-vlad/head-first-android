d1bb23b867e76abb070c5715316b841b4de1a81a
vlad <ysc.vlad@gmail.com>
2018-11-06 00:02:52 +0300

    add toasts and scrollView

--- layouts/LinearLayout/app/src/main/java/com/example/linearlayout/MainActivity.java
+++ layouts/LinearLayout/app/src/main/java/com/example/linearlayout/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.linearlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickShowToasts(View view) {
+        CharSequence message = "Hello, i am toasts";
+
+        int time = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(this, message, time);
+
+        toast.show();
     }
 }
